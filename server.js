8d1d00a389f986a4883d713acd6498ff5fc6b9db
Shivam Singh Mer <mershivam02@gmail.com>
2025-12-03 11:59:21 +0530

    refactor: use regex literal for catch-all route pattern

--- server.js
+++ server.js
@@ -77,6 +77,6 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('(.*)', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
